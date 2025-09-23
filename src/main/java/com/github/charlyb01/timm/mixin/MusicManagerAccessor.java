d94682c296a1bd4d69d2cdb9a4649d1baf561803
CrawKatt <man849916@gmail.com>
2025-09-22 23:57:32 -0300

    Fix music overlap on structure music

---
+++ src/main/java/com/github/charlyb01/timm/mixin/MusicManagerAccessor.java
@@ -0,0 +1,12 @@
+package com.github.charlyb01.timm.mixin;
+
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.sounds.MusicManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MusicManager.class)
+public interface MusicManagerAccessor {
+    @Accessor("currentMusic")
+    void setCurrent(SoundInstance soundInstance);
+}
